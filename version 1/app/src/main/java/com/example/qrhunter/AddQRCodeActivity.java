b9b41be076df49fa0c2d859592679e7095937eb5
GemmaryChang <gemmary@ualberta.ca>
2022-03-12 11:05:31 -0700

    Scan QR code camera has been set up

---
+++ version 1/app/src/main/java/com/example/qrhunter/AddQRCodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.qrhunter;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddQRCodeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_profile_layout);
+    }
+}
--- version 1/app/src/main/java/com/example/qrhunter/ScanQRcodeActivity.java
+++ version 1/app/src/main/java/com/example/qrhunter/ScanQRcodeActivity.java
@@ -1,13 +1,32 @@
 package com.example.qrhunter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScanQRcodeActivity extends AppCompatActivity {
+
+    Button scanbtn;
+    public static TextView scantext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up_layout);
+        setContentView(R.layout.scan_qr_code_layout);
+
+        scantext=(TextView)findViewById(R.id.scantext);
+        scanbtn=(Button) findViewById(R.id.camera_button);
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),scannerViewActivity.class));
+            }
+        });
     }
 }
+
+
