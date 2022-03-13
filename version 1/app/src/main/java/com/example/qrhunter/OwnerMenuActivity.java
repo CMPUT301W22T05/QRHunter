6cace5101d8e44ed18ede4a35c05575bb975d08b
Wenhao You <wyou1@ualberta.ca>
2022-03-13 16:03:02 -0600

    wenhao for delete PlayerQRinformation.java & SearchInOwnerMenu.java and update OwnerMenuActivity.java

--- version 1/app/src/main/java/com/example/qrhunter/OwnerMenuActivity.java
+++ version 1/app/src/main/java/com/example/qrhunter/OwnerMenuActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class OwnerMenuActivity extends AppCompatActivity {
     ImageButton SearchButton;
     Button RankingButton;
+    TextView SearchUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,9 @@ public class OwnerMenuActivity extends AppCompatActivity {
 
         SearchButton = findViewById(R.id.search_user_name_Button);
         RankingButton = findViewById(R.id.get_ranking_button);
+        SearchUserName = findViewById(R.id.search_user_name);
+        // open the firebase and connect
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
         RankingButton.setOnClickListener(new View.OnClickListener() {
@@ -35,9 +46,32 @@ public class OwnerMenuActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent JumpToPlayerinformationpage = new Intent();
-                JumpToPlayerinformationpage.setClass(OwnerMenuActivity.this,PlayerQRinformation.class);
-                startActivity(JumpToPlayerinformationpage);
+                final String Username = SearchUserName.getText().toString();
+                if (Username.length() == 0){  // user did not enter any information in
+                    Toast.makeText(OwnerMenuActivity.this,"Username cannot be empty",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    DocumentReference noteRef = db.collection("Player").document(Username);
+                    noteRef.get()
+                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                @Override
+                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                    if (documentSnapshot.exists()) {
+                                        Intent JumpToPersonalRank = new Intent();
+                                        JumpToPersonalRank.setClass(OwnerMenuActivity.this, PersonalRank.class);
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("UserName", Username);
+                                        JumpToPersonalRank.putExtras(bundle);
+                                        startActivity(JumpToPersonalRank);
+                                    }
+                                    else {
+                                        Toast.makeText(OwnerMenuActivity.this, "Username not found", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            });
+
+                }
             }
         });
 
