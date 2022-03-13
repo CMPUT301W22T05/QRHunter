2b803e0f18512dd9ef9543de76f49db3dff64ea9
Wenhao You <wyou1@ualberta.ca>
2022-03-13 15:26:21 -0600

    wenhao for SearchInOwnerMenu.java and PersonalRank.java (continued

---
+++ version 1/app/src/main/java/com/example/qrhunter/PersonalRank.java
@@ -0,0 +1,16 @@
+package com.example.qrhunter;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PersonalRank extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_qr_rank_layout);
+
+    }
+}
