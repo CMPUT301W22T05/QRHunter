955a6186f51964eaae246834e6077fc191635d24
Sun-ning1 <chinaisrising999@gmail.com>
2022-03-14 12:04:26 -0600

    null id display

--- version 1/app/src/main/java/com/example/qrhunter/PersonalRank.java
+++ version 1/app/src/main/java/com/example/qrhunter/PersonalRank.java
@@ -4,10 +4,22 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import com.google.firebase.database.ChildEventListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
 
 public class PersonalRank extends AppCompatActivity {
+
+
+    private String Null;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -19,8 +31,13 @@ public class PersonalRank extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(OwnerMenuActivity.EXTRA_MESSAGE);
         TextView PersonalName = findViewById(R.id.personal_rank_TextView);
-        PersonalName.setText(message+" 's QR Codes");
+        if (message == Null) {
+            PersonalName.setText("UnknowID player's QR Codes");
+
+        } else {
+            PersonalName.setText(message + "'s QR Codes");
 
 
+        }
     }
 }
