ba649d548a1524e2005b3b8621b40354459d5fbf
rahuluniv <rahulunivoffical@gmail.com>
2022-03-30 16:09:02 -0600

    location working

--- version 1/app/src/main/java/com/example/qrhunter/ScanQRcodeActivity.java
+++ version 1/app/src/main/java/com/example/qrhunter/ScanQRcodeActivity.java
@@ -1,52 +1,80 @@
 package com.example.qrhunter;
 
-import static com.example.qrhunter.MainActivity.user;
-
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import static com.google.common.math.IntMath.pow;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.util.HashMap;
+
+
 
 public class ScanQRcodeActivity extends AppCompatActivity {
 
     Button scanbtn;
     Button upload;
+    Button Location;
     EditText comments;
     public static TextView scantext;
     public int codeworth;
+    private int QRHash;
     String data = "";
+    SupportMapFragment smf;
+    FusedLocationProviderClient client;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_qr_code_layout);
-
         // initialize the setting of textview, button and database
         scantext = (TextView) findViewById(R.id.scantext);
         scanbtn = (Button) findViewById(R.id.camera_button);
         upload = (Button) findViewById(R.id.upload_comments_button);
+        Location = (Button) findViewById(R.id.location_button);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         comments = (EditText) findViewById(R.id.editText_comments);
         // set the functionality of switching activity of scan button
+
         scanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,15 +83,16 @@ public class ScanQRcodeActivity extends AppCompatActivity {
 
             }
         });
+
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String title = scantext.getText().toString();
                 String description = comments.getText().toString();
-                codeworth = calculateWorth(scantext.getText().toString());
+                codeworth = calculateWorth(title);
                 SharedPreferences MyProfileData = getSharedPreferences("data", 0);
                 String username = MyProfileData.getString("username", null);
-                Note note = new Note(title, description,codeworth);
+                Note note = new Note(title, description, codeworth);
                 final CollectionReference collectionReference = db.collection("Player");
                 DocumentReference noteRef = db.collection("Player").document(username);
                 collectionReference.document(username)
@@ -96,32 +125,84 @@ public class ScanQRcodeActivity extends AppCompatActivity {
 
             }
         });
+        Location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+                smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+                client = LocationServices.getFusedLocationProviderClient(ScanQRcodeActivity.this);
+
+                Dexter.withContext(getApplicationContext())
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                getmylocation();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
 
     }
+    public void getmylocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                smf.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+                        MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("You are here...!!");
+
+                        googleMap.addMarker(markerOptions);
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17));
+                    }
+                });
+            }
+        });
+    }
+
 
     public int calculateWorth(String qrCodeContent) {
         try {
-
             // calculate sha-256
             // Citation: https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha256-in-java
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hash = md.digest(qrCodeContent.getBytes(StandardCharsets.UTF_8));
-
             final StringBuilder hashStr = new StringBuilder(hash.length);
             for (byte hashByte : hash)
                 hashStr.append(Integer.toHexString(255 & hashByte));
-
             // Hashed string here
             final String hashedContent = hashStr.toString();
-
             final char[] hashedArray = hashedContent.toCharArray();
-
             // Calculating String
             char[] codeArray = new char[hashedContent.length()];
             int[] intCodeArray = new int[hashedContent.length() + 1];
             int comparer = 0;
             int codeWorth = 0;
-
             int counter = 0;
 
             // Copy char by char into array
@@ -162,7 +243,7 @@ public class ScanQRcodeActivity extends AppCompatActivity {
                     comparer = intCodeArray[i];
                 }
             }
-        return codeWorth;
+            return codeWorth;
 
 
         } catch (Exception e) {
@@ -170,10 +251,9 @@ public class ScanQRcodeActivity extends AppCompatActivity {
             System.err.println(e.getMessage());
         }
         return 0;
+
+
     }
-
-
-
 }
 
 
