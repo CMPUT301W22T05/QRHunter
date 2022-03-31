05ccf1294b90d8ff61157eaa44e5dfb1f110e913
Wenhao You <wyou1@ualberta.ca>
2022-03-30 18:29:06 -0600

    wenhao for continued owner part

--- version 1/app/src/main/java/com/example/qrhunter/OwnerMenuActivity.java
+++ version 1/app/src/main/java/com/example/qrhunter/OwnerMenuActivity.java
@@ -3,23 +3,40 @@ package com.example.qrhunter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import org.w3c.dom.Text;
 
 public class OwnerMenuActivity extends AppCompatActivity {
     ImageButton SearchButton;
     Button RankingButton;
     TextView SearchUserName;
     TextView PersonalName;
+    ListView ListResult;
+
+    private DatabaseReference mUserDatabase;
+
     public static final String EXTRA_MESSAGE = "com.example.qrhunter.MESSAGE";
 
     @Override
@@ -27,16 +44,42 @@ public class OwnerMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.owner_menu_layout);
 
+        //
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
         // initialize all the buttons and textview in owner_menu_layout
-        SearchButton = findViewById(R.id.search_user_name_Button);
-        RankingButton = findViewById(R.id.get_ranking_button);
-        SearchUserName = findViewById(R.id.search_user_name);
+        SearchButton = (ImageButton) findViewById(R.id.search_user_name_Button);
+        RankingButton = (Button) findViewById(R.id.get_ranking_button);
+        SearchUserName = (EditText) findViewById(R.id.search_user_name);
         // open the firebase and connect
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         // update information in personal_qr_rank_layout.xml
-        PersonalName = findViewById(R.id.personal_rank_TextView);
+        PersonalName = (TextView) findViewById(R.id.personal_rank_TextView);
+
+        // listview
+        ListResult = (ListView) findViewById(R.id.ranking_total_score_list);
+//        ListResult.setHasFixedSize;
+//        ListResult.setLayoutManger
+
+
+        SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firebaseUserSearch();
+            }
+        });
+
+
+
+
+
+
+
+
 
         // set the functionality of switching activity of ranking button
+        // 点击排序按钮 --> 再跳转
         RankingButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -47,44 +90,89 @@ public class OwnerMenuActivity extends AppCompatActivity {
             }
         });
 
-        // set the functionality of search button
-        SearchButton.setOnClickListener(new View.OnClickListener() {
+//        // set the functionality of search button
+//        // 老版本： 点击搜索按钮 --> 再跳转
+//        SearchButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                final String Username = SearchUserName.getText().toString();
+//                if (Username.length() == 0){  // user did not enter any information in
+//                    Toast.makeText(OwnerMenuActivity.this,"Username cannot be empty",Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//                    DocumentReference noteRef = db.collection("Player").document(Username);
+//                    noteRef.get()
+//                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                                @Override
+//                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                                    if (documentSnapshot.exists()) {  // can find player name in database
+////                                        Intent JumpToPersonalRank = new Intent();
+//////                                        JumpToPersonalRank.setClass(OwnerMenuActivity.this, PersonalRank.class);
+////                                        startActivity(JumpToPersonalRank);
+//                                        // put user's name to next page
+//                                        String name = documentSnapshot.getString("Name");
+//                                        Intent SendToNextTitle = new Intent(OwnerMenuActivity.this, PersonalRank.class);
+//                                        SendToNextTitle.putExtra(EXTRA_MESSAGE, name);
+//                                        startActivity(SendToNextTitle);
+//
+//                                    }
+//                                    else {  // can't find player name in database
+//                                        Toast.makeText(OwnerMenuActivity.this, "Username not found", Toast.LENGTH_SHORT).show();
+//                                    }
+//                                }
+//
+//                            });
+//
+//
+//                }
+//
+//            }
+//        });
+//
 
+
+    }
+
+    private void firebaseUserSearch() {
+
+        FirebaseRecyclerAdapter<RankOnOwnerPage, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<RankOnOwnerPage, UsersViewHolder>(
+                RankOnOwnerPage.class,
+                R.layout.ranking_list_in_owner_layout,
+                UsersViewHolder.class,
+                mUserDatabase
+
+        ) {
             @Override
-            public void onClick(View view) {
-                final String Username = SearchUserName.getText().toString();
-                if (Username.length() == 0){  // user did not enter any information in
-                    Toast.makeText(OwnerMenuActivity.this,"Username cannot be empty",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    DocumentReference noteRef = db.collection("Player").document(Username);
-                    noteRef.get()
-                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                @Override
-                                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                    if (documentSnapshot.exists()) {  // can find player name in database
-//                                        Intent JumpToPersonalRank = new Intent();
-////                                        JumpToPersonalRank.setClass(OwnerMenuActivity.this, PersonalRank.class);
-//                                        startActivity(JumpToPersonalRank);
-                                        // put user's name to next page
-                                        String name = documentSnapshot.getString("Name");
-                                        Intent SendToNextTitle = new Intent(OwnerMenuActivity.this, PersonalRank.class);
-                                        SendToNextTitle.putExtra(EXTRA_MESSAGE, name);
-                                        startActivity(SendToNextTitle);
+            protected void populateViewHolder(UsersViewHolder viewHolder, RankOnOwnerPage model, int position) {
 
-                                    }
-                                    else {  // can't find player name in database
-                                        Toast.makeText(OwnerMenuActivity.this, "Username not found", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-
-                            });
-
-
-                }
-
+                viewHolder.setDetails(model.getName(), model.getTotalScore());
             }
-        });
+
+        };
+        ListResult.setAdapter((ListAdapter) firebaseRecyclerAdapter);
+    }
+
+    // View Holder Class
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setDetails(String playerName, String playerScore) {
+            TextView PlayerName = (TextView) mView.findViewById(R.id.player_name_textview);
+            TextView TotalScore = (TextView) mView.findViewById(R.id.total_score_textview);
+
+            PlayerName.setText(playerName);
+            TotalScore.setText(playerScore);
+
+
+        }
+
 
 
 
