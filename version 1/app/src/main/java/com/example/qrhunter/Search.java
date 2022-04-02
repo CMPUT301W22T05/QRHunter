ae57036afce063f043b468c0b56c4bcb9251faa4
rahuluniv <rahulunivoffical@gmail.com>
2022-04-01 20:21:09 -0600

    early version of map

--- version 1/app/src/main/java/com/example/qrhunter/Search.java
+++ version 1/app/src/main/java/com/example/qrhunter/Search.java
@@ -1,19 +1,54 @@
 package com.example.qrhunter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+
 public class Search extends AppCompatActivity {
     ImageButton SearchPlayerButton;
     ImageButton camera;
     EditText UsernameSearchEdit;
+    SupportMapFragment sm;
+    FusedLocationProviderClient cl;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +64,11 @@ public class Search extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 final String Username = UsernameSearchEdit.getText().toString();
-                if (Username.length() == 0){
+                if (Username.length() == 0) {
                     Toast.makeText(Search.this, "Username can not be empty!", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     Intent JumpToPlayerProfile = new Intent();  // for player
-                    JumpToPlayerProfile.setClass(Search.this,SearchInfo.class);
+                    JumpToPlayerProfile.setClass(Search.this, SearchInfo.class);
                     Bundle bundle = new Bundle();
                     bundle.putString("UserName", Username);
                     JumpToPlayerProfile.putExtras(bundle);
@@ -42,6 +77,80 @@ public class Search extends AppCompatActivity {
 
             }
         });
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        sm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.small_map);
+        cl= LocationServices.getFusedLocationProviderClient(Search.this);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("QRCODES")
+//                .document()
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    private static final String TAG = "location";
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                                MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here...!!");
+//                                small_map.addMarker(markerOptions);
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+
+
+
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        getmylocation();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
 
     }
+    public void getmylocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Task<Location> task = cl.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                sm.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here...!!");
+                        googleMap.addMarker(markerOptions);
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                    }
+                });
+            }
+        });
+    }
 }
+
